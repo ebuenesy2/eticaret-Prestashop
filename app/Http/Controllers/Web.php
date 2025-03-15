3c7d4103674c2db13924f5ba94ead015658a69df
EBU ENES YILDIRIM <86044516+ebuenesy2@users.noreply.github.com>
2025-03-15 21:05:25 +0300

    ok

--- app/Http/Controllers/Web.php
+++ app/Http/Controllers/Web.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use DB; //Veri Tabanı İşlemleri
+use CURLFile; //! Curl 
 
 //! Zaman
 use Carbon\Carbon;
@@ -26,12 +27,81 @@ class Web extends Controller
         
         try {
             
-            echo "Dil:"; echo $site_lang;  echo "<br/>";
-            echo "web test"; echo "<br/>"; 
+            //echo "Dil:"; echo $site_lang;  echo "<br/>";
 
-            //! Zaman
-            $currentTime = Carbon::now(); //! 2023-02-23 08:05:04
-            echo "currentTime:"; echo $currentTime;  echo "<br/>";
+            //url
+            $url="https://marwella1.eldenpazar.com/api/products"; 
+
+            // //! Bearer
+            // $token ="yildirimdev01"; //! Token
+            // $authorization = "Authorization: Bearer ".$token; // Header BearerToken
+            // $headers = array("Content-Type" => "multipart/form-data" , $authorization );
+
+
+            //! ----- Curl  ----------
+            $curl = curl_init(); //! Curl Başlatıyor
+            curl_setopt($curl, CURLOPT_URL, $url);
+            curl_setopt($curl, CURLOPT_CONNECTTIMEOUT, 30); //! Timeout
+            //curl_setopt($curl, CURLOPT_HTTPHEADER, $headers);
+            curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, FALSE); // SSL important
+            curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
+            curl_setopt($curl, CURLOPT_POST, 0);
+
+            curl_setopt($curl, CURLOPT_HTTP_VERSION, CURL_HTTP_VERSION_1_1);
+            curl_setopt($curl, CURLOPT_TIMEOUT, 900); //! Timeout
+            //! ----- Curl Son  -----
+
+
+            //veri okunuyor
+            $output = curl_exec($curl);
+            $status = 0;
+            
+            //sorun varsa
+            if($e=curl_error($curl)) { echo $e; die(); }
+            else
+            {  
+                // Json verisine dönüştür
+                //array oluştur
+                $decoded=json_decode($output,true);
+                echo "<pre>"; print_r($decoded); die();
+
+                //okuma * dönüştürme işlemleri
+                $title=$decoded["title"];
+                $table=$decoded["table"];
+                $status=$decoded["status"];
+                //$size=$decoded["size"];
+
+                //iç içe json
+                $DB_sayisi=count($decoded["DB"]);
+                //$DB_id_cok=$decoded["DB"][0]["id"];
+                //$DB_id=$decoded["DB"]["id"];
+
+                //Ekran Çıktısı
+                echo "Veri Çekiyor";
+                echo "<br>";
+                echo "title: ".$title;
+                echo "<br>";
+                echo "table: ".$table;
+                echo "<br>";
+                
+                echo "DB - Sayısı : ".$DB_sayisi;
+                echo "<br>";
+                //echo "DB - ID [0] : ".$DB_id_cok;
+                echo "<br>";
+
+            }
+
+            //kapat
+            curl_close($curl);
+
+
+            //! Return
+            $response = array( 'status' => $status == 1 ? 'success' : 'error' );
+            
+            print_r($response); 
+            //return response()->json($response);
+
+            
             
         } catch (\Throwable $th) {  throw $th; }
 
